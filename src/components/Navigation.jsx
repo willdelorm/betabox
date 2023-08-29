f5dfbdd5198ff4bb5ea494d9a127e4cd2a5ac76d
Will Delorm <willdelorm@gmail.com>
2023-08-28 18:05:09 -0700

    Revert nav menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -15,8 +15,8 @@ const Navigation = ({ theme }) => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            {/* <Nav.Link href="/home">Home</Nav.Link> */}
-            {/* <Nav.Link href="/">Sign Out</Nav.Link> */}
+            <Nav.Link href="/home">Home</Nav.Link>
+            <Nav.Link href="/">Sign Out</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
