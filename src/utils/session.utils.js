893ee3e3e6472c0fca0d1769ffefd442810ac10a
Will Delorm <willdelorm@gmail.com>
2023-08-29 13:59:41 -0700

    create utility functions for session data

---
+++ src/utils/session.utils.js
@@ -0,0 +1,27 @@
+const getProblemsCount = (climbs) => {
+  return climbs.length ? climbs.length : 0;
+};
+const getVSum = (climbs) => {
+  return climbs.length
+    ? climbs.reduce((acc, climb) => (acc += climb.grade), 0)
+    : 0;
+};
+const getAvgV = (climbs) => {
+  return climbs.length
+    ? Math.floor(
+        climbs.reduce((acc, climb) => (acc += climb.grade), 0) / climbs.length
+      )
+    : 0;
+};
+const getProjectsCount = (climbs) => {
+  return climbs.filter((climb) => climb.style === "Project").length;
+};
+
+export const sessionStats = (climbs) => {
+  return {
+    problemsCount: getProblemsCount(climbs),
+    vSum: getVSum(climbs),
+    avgV: getAvgV(climbs),
+    projectsCount: getProjectsCount(climbs),
+  };
+};
