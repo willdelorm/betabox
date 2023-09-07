78ec42f4e3ffa7cf37ae327f0a07655d321b877b
Will Delorm <willdelorm@gmail.com>
2023-09-07 12:47:35 -0700

    implement user sign out

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { signOutAuthUser } from "../utils/firebase.utils";
+
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 const Navigation = ({ theme }) => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOutAuthUser();
+    navigate("/");
+  };
+
   return (
     <Navbar
       expand="lg"
@@ -16,7 +26,7 @@ const Navigation = ({ theme }) => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/">Sign Out</Nav.Link>
+            <Nav.Link onClick={handleSignOut}>Sign Out</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
