c2f7b5e16d118e327813a8cce68dd9c3db152564
Will Delorm <willdelorm@gmail.com>
2023-09-18 13:10:37 -0700

    update stats preview string

--- src/components/SessionsListItem.jsx
+++ src/components/SessionsListItem.jsx
@@ -1,9 +1,37 @@
 import { ListGroup } from "react-bootstrap";
 import { sessionStats } from "../utils/session.utils";
 
+const calculateSessionTime = (startTime, endTime) => {
+  const timeStart = new Date(startTime.seconds * 1000);
+  let timeEnd = new Date(endTime.seconds * 1000);
+
+  // Adjust if times are on opposite sides of midnight
+  if (timeEnd < timeStart) {
+    timeEnd.setDate(timeEnd.getDate() + 1);
+  }
+
+  const diff = timeEnd - timeStart;
+
+  let msec = diff;
+  const hh = Math.floor(msec / 1000 / 60 / 60);
+  msec -= hh * 1000 * 60 * 60;
+  const mm = Math.floor(msec / 1000 / 60);
+  msec -= mm * 1000 * 60;
+  const hhStr = hh > 0 ? `${hh} hours ` : "";
+  const mmStr = mm > 0 ? `${mm} minutes ` : "";
+  const elapsedTimeStr = hhStr + mmStr;
+
+  return elapsedTimeStr.length ? elapsedTimeStr : "Short";
+};
+
 const SessionsListItem = ({ session, handleClick }) => {
-  const { title, climbs } = session;
+  const { climbs, endTime, startTime, title } = session;
   const { problemsCount, avgV, projectsCount } = sessionStats(climbs);
+  const sessionTime = calculateSessionTime(startTime, endTime);
+
+  const statsPreview = `${sessionTime} • ${problemsCount} problems • V${avgV} average ${
+    projectsCount ? `• ${projectsCount} projects` : ""
+  }`;
 
   return (
     <ListGroup.Item
@@ -12,7 +40,7 @@ const SessionsListItem = ({ session, handleClick }) => {
     >
       <div className="flex-grow-1">
         <h3 className="fs-7">{title}</h3>
-        <p className="fs-8 m-0">{`2 hours • ${problemsCount} problems • Average V${avgV} • ${projectsCount} project`}</p>
+        <p className="fs-8 m-0">{statsPreview}</p>
       </div>
       <div className="fs-8">
         <i className="fa-solid fa-chevron-right"></i>
